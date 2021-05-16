42c927d42c33e9324c21102024e292917dedcc94
Alex Katopodis <alexkatopodis@live.com>
2021-05-16 02:38:56 -0300

    Incluido Rotas Trending, Movies, Series, Search

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
+import { BrowserRouter, Switch} from 'react-router-dom';
 import Header from "./components/Header/Header";
 import './App.css';
+import SimpleBottomNavigation from './components/MainNav';
+import Container from '@material-ui/core/Container';
+import Trending from './components/Pages/Trending/Trending';
+import Movies from './components/Pages/Movies/Movies';
+import Series from './components/Pages/Series/Series';
+import Search from './components/Pages/Search/Search';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
 
-    <>
+    <BrowserRouter>
       <Header />
-    <div className="App">feff</div>
-    </>
+    <div className="App">
+        <Container>
+          <Switch>
+             <Route path='/' component={Trending} exact/>
+             <Route path='/movies' component={Movies} />
+             <Route path='/series' component={Series} />
+             <Route path='/search' component={Search} />
+          </Switch>
+        </Container>
+
+    </div>
+
+
+    <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,12 @@
+/* eslint-disable jsx-a11y/alt-text */
 import './Header.css';
 
 
 const Header = () => {
-    return <span className='header'>🖖 HUB AE NERD 🖖</span>
+    return <div>         
+         <span className='header'>🖖 HUB AE NERD 🖖</span>
+          </div>
+    
 };
 
 export default Header
